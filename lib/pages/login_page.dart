57bd8ad157cedafa982337ed8c19b70bbb4c7ba4
Gabriel Andreatto Alves <gabriel.andreatto.1@gmail.com>
2020-01-02 16:59:47 -0300

    Flutter App Carros: form basic login TextEditingController

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  final controleLogin = TextEditingController();
+  final controleSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,38 +19,65 @@ class LoginPage extends StatelessWidget {
       padding: EdgeInsets.all(16),
       child: ListView(
         children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Login",
-              hintText: "Login",
-            ),
+          _textFormField(
+            "LOGIN",
+            "Login",
+            controller: controleLogin,
           ),
           SizedBox(height: 16),
-          TextFormField(
-            decoration: InputDecoration(
-              labelText: "Senha",
-              hintText: "Senha",
-            ),
+          _textFormField(
+            "SENHA",
+            "Senha",
             obscureText: true,
+            controller: controleSenha,
           ),
           SizedBox(height: 16),
           Container(
             margin: EdgeInsets.only(top: 8),
             height: 46,
-            child: RaisedButton(
-              color: Colors.blue,
-              child: Text(
-                "Clicar",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                ),
-              ),
-              onPressed: () => {},
+            child: _raisedButton(
+              "LOGIN",
+              () {
+                _onClickLogin();
+              },
             ),
           ),
         ],
       ),
     );
+  }
+
+  TextFormField _textFormField(String param, String hint,
+      {bool obscureText = false, TextEditingController controller}) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: param,
+        hintText: hint,
+      ),
+    );
+  }
+
+  RaisedButton _raisedButton(String name, Function onPressed) {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text(
+        name,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 22,
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+
+  _onClickLogin() {
+    String login = controleLogin.text;
+    String senha = controleSenha.text;
+
+    // controleLogin.text = novo nome
+    print("Login: $login senha: $senha");
   }
 }
